638bab6b440051bff76e9e8ecae34b08bd42d37f
xavpav9 <xtondelier@gmail.com>
2025-12-31 10:32:30 +0000

    Use std arrays instead of c-style arrays
    This is so that the vector of points can store each point as its own
    array. This makes it easier to work with the points.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <limits>
 #include <algorithm>
 #include <cmath>
+#include <array>
 
 void clearScreen() {
   std::cout << "\033[2J\033[1;1H";
@@ -16,8 +17,8 @@ void emptyBuffer(char buffer[], int width, int height) {
   }
 }
 
-std::vector<int> getPoints(int startPos[2], int endPos[2]) {
-  std::vector<int> points;
+std::vector<std::array<int,2>> getPoints(std::array<int, 2> startPos, std::array<int, 2> endPos) {
+  std::vector<std::array<int,2>> points;
   int axis;
   float gradient = (float)(startPos[1] - endPos[1]) / (float)(startPos[0] - endPos[0]);
 
@@ -27,8 +28,8 @@ std::vector<int> getPoints(int startPos[2], int endPos[2]) {
     axis = 0;
   }
 
-  int initialPos[2];
-  int finishPos[2];
+  std::array<int, 2> initialPos;
+  std::array<int, 2> finishPos;
 
   if (startPos[axis] <= endPos[axis]) {
     for (int i = 0; i < 2; ++i) {
@@ -45,14 +46,14 @@ std::vector<int> getPoints(int startPos[2], int endPos[2]) {
   if (axis == 0) {
     for (int x = initialPos[0]; x <= finishPos[0]; ++x) {
       int y = (int)(gradient * (x - initialPos[0]) + initialPos[1]);
-      points.push_back(x);
-      points.push_back(y);
+      std::array<int, 2> point = {x, y};
+      points.push_back(point);
     }
   } else {
     for (int y = initialPos[1]; y <= finishPos[1]; ++y) {
       int x = (int)((y - initialPos[1]) / gradient + initialPos[0]);
-      points.push_back(x);
-      points.push_back(y);
+      std::array<int, 2> point = {x, y};
+      points.push_back(point);
     }
   }
 
@@ -68,64 +69,66 @@ void drawBuffer(char buffer[], int width, int height) {
   }
 }
 
-void addPoint(char buffer[], int width, int height, int point[]) {
-  point[0] += int(width / 2);
-  point[1] += int(height / 2);
+void addPoint(char buffer[], int width, int height, std::array<int, 2> point) {
+  point[0] += (int)(width / 2);
+  point[1] += (int)(height / 2);
   if (point[0] < width && point[0] >= 0 && point[1] >= 0 && point[1] < height) {
     buffer[width * point[1] + point[0]] = '@';
   }
 }
 
-void drawLine(char buffer[], int width, int height, int start[], int end[]) {
-  std::vector<int> points = getPoints(start, end);
+void drawLine(char buffer[], int width, int height, std::array<int, 2> start, std::array<int, 2> end) {
+  std::vector<std::array<int,2>> points = getPoints(start, end);
 
-  for (int i = 0; i < points.size(); i += 2) {
-    int point[2] = {points[i], points[i+1]};
-    addPoint(buffer, width, height, point);
+  for (int i = 0; i < points.size(); ++i) {
+    addPoint(buffer, width, height, points[i]);
   }
 }
 
 class Triangle {
 public:
-  float vertices[6];
-  Triangle(int points[6]) {
-    for (int i = 0; i < 6; ++i) {
-      vertices[i] = (float)points[i];
+  std::array<std::array<float, 2>, 3> vertices;
+  Triangle(std::array<std::array<int, 2>, 3> points) {
+    for (int i = 0; i < 3; ++i) {
+      for (int j = 0; j < 2; ++j) {
+        vertices[i][j] = (float)points[i][j];
+      }
     }
   }
 
   void draw(char buffer[], int width, int height) {
-    int top = (int)std::min(vertices[1], std::min(vertices[3], vertices[5]));
-    int bottom = (int)std::max(vertices[1], std::max(vertices[3], vertices[5]));
+    int top = (int)std::min(vertices[0][1], std::min(vertices[1][1], vertices[2][1]));
+    int bottom = (int)std::max(vertices[0][1], std::max(vertices[1][1], vertices[2][1]));
 
-    int vertex1[2] = {(int)vertices[0], (int)vertices[1]};
-    int vertex2[2] = {(int)vertices[2], (int)vertices[3]};
-    int vertex3[2] = {(int)vertices[4], (int)vertices[5]};
-    std::vector<int> points = getPoints(vertex1, vertex2);
-    std::vector<int> points2 = getPoints(vertex1, vertex3);
+
+    std::array<int, 2> vertex1 = {(int)vertices[0][0], (int)vertices[0][1]};
+    std::array<int, 2> vertex2 = {(int)vertices[1][0], (int)vertices[1][1]};
+    std::array<int, 2> vertex3 = {(int)vertices[2][0], (int)vertices[2][1]};
+    std::vector<std::array<int,2>> points = getPoints(vertex1, vertex2);
+    std::vector<std::array<int,2>> points2 = getPoints(vertex1, vertex3);
     points.insert(points.end(), points2.begin(), points2.end());
-    std::vector<int> points3 = getPoints(vertex3, vertex2);
+    std::vector<std::array<int,2>> points3 = getPoints(vertex3, vertex2);
     points.insert(points.end(), points3.begin(), points3.end());
 
     for (int y = top; y <= bottom; ++y) {
       int minX = std::numeric_limits<int>::max();
       int maxX = std::numeric_limits<int>::min();
 
-      for (int i = 0; i < points.size(); i += 2) {
-        if (points[i+1] == y) {
-          if (points[i] < minX) {
-            minX = points[i];
+      for (int i = 0; i < points.size(); ++i) {
+        if (points[i][1] == y) {
+          if (points[i][0] < minX) {
+            minX = points[i][0];
           }
-          if (points[i] > maxX) {
-            maxX = points[i];
+          if (points[i][0] > maxX) {
+            maxX = points[i][0];
           }
           points.erase(points.begin() + i);
-          points.erase(points.begin() + i);
-          i -= 2;
+          i--;
         }
       }
+
       for (int x = minX; x <= maxX; ++x) {
-        int point[2] = {x, y};
+        std::array<int, 2> point = {x, y};
         addPoint(buffer, width, height, point);
       }
     }
@@ -139,10 +142,14 @@ int main() {
   char buffer[width * height];
   emptyBuffer(buffer, width, height);
 
-  int start[2] = {0,0};
-  int end[2] = {10,20};
-  int points[6] = {-20,0,0,0,0,-20};
-  int points2[6] = {0,20,0,0,20,0};
+  std::array<int, 2> point1 = {-20, 0};
+  std::array<int, 2> point2 = {0, 20};
+  std::array<int, 2> point3 = {0, -20};
+  std::array<int, 2> point4 = {20, 0};
+  std::array<int, 2> point5 = {0, 20};
+  std::array<int, 2> point6 = {0, -20};
+  std::array<std::array<int, 2>, 3> points = {point1, point2, point3};
+  std::array<std::array<int, 2>, 3> points2 = {point4, point5, point6};
   Triangle trig1(points);
   Triangle trig2(points2);
 
@@ -154,10 +161,10 @@ int main() {
 
     for (int o = 0; o < sizeof(trigs) / sizeof(Triangle); ++o) {
       Triangle trig = trigs[o];
-      for (int i = 0; i < 6; i += 2) {
-        float oldX = trig.vertices[i];
-        trig.vertices[i] = std::cos(0.1) * trig.vertices[i] - std::sin(0.1) * trig.vertices[i + 1];
-        trig.vertices[i + 1] = std::sin(0.1) * oldX + std::cos(0.1) * trig.vertices[i + 1];
+      for (int i = 0; i < 3; ++i) {
+        float oldX = trig.vertices[i][0];
+        trig.vertices[i][0] = std::cos(0.1) * trig.vertices[i][0] - std::sin(0.1) * trig.vertices[i][1];
+        trig.vertices[i][1] = std::sin(0.1) * oldX + std::cos(0.1) * trig.vertices[i][1];
       }
       trig.draw(buffer, width, height);
       trigs[o] = trig;
